d8d8264de7b3169e796b1e04fb64b941f13c1757
kla <la.kien@gmail.com>
2009-06-24 02:11:15 -0400

    converted to smoothie tests

--- test/InflectorTest.php
+++ test/InflectorTest.php
@@ -1,24 +1,28 @@
 <?php
+namespace Smoothie;
+
 include 'helpers/config.php';
 require_once dirname(__FILE__) . '/../lib/Inflector.php';
 
-class InflectorTest extends SnakeCase_PHPUnit_Framework_TestCase
-{
-	public function set_up()
-	{
-		$this->inflector = ActiveRecord\Inflector::instance();
-	}
+context("Inflector", function() {
+	setup(function() { transient()->inflector = \ActiveRecord\Inflector::instance(); });
 
-	public function test_underscorify()
-	{
-		$this->assert_equals('rm_name_bob',$this->inflector->variablize('rm--name  bob'));
-		$this->assert_equals('One_Two_Three',$this->inflector->underscorify('OneTwoThree'));
-	}
+	should("variablize", function() {
+		assert_equals("rm_name_bob", transient()->inflector->variablize("rm--name  bob"));
+	});
 
-	public function test_tableize()
-	{
-		$this->assert_equals('angry_people',$this->inflector->tableize('AngryPerson'));
-		$this->assert_equals('my_sqls',$this->inflector->tableize('MySQL'));
-	}
-};
+	should("underscorify", function() {
+		assert_equals('One_Two_Three', transient()->inflector->underscorify("OneTwoThree"));
+	});
+
+	context("tableize", function() {
+		should("a camel cased string", function() {
+			assert_equals("angry_people", transient()->inflector->tableize("AngryPerson"));
+		});
+
+		should("a camel cased string with consecutive caps", function() {
+			assert_equals("my_sqlservers", transient()->inflector->tableize("MySQLServer"));
+		});
+	});
+});
 ?>
--- test/UtilsTest.php
+++ test/UtilsTest.php
@@ -1,87 +1,84 @@
 <?php
-include 'helpers/config.php';
-
+namespace Smoothie;
 use ActiveRecord as AR;
 
-class UtilsTest extends SnakeCase_PHPUnit_Framework_TestCase
-{
-	public function set_up()
-	{
-		$this->object_array = array(null,null);
-		$this->object_array[0] = new stdClass();
-		$this->object_array[0]->a = "0a";
-		$this->object_array[0]->b = "0b";
-		$this->object_array[1] = new stdClass();
-		$this->object_array[1]->a = "1a";
-		$this->object_array[1]->b = "1b";
+include 'helpers/config.php';
 
-		$this->array_hash = array(
-			array("a" => "0a", "b" => "0b"),
-			array("a" => "1a", "b" => "1b"));
-	}
+context("Utils", function() {
+	context("collect", function() {
+		context("with an array of objects", function() {
+			setup(function() {
+				$object_array = array();
+				$object_array[0] = new \stdClass();
+				$object_array[0]->a = "0a";
+				$object_array[0]->b = "0b";
+				$object_array[1] = new \stdClass();
+				$object_array[1]->a = "1a";
+				$object_array[1]->b = "1b";
+				transient()->object_array = $object_array;
+			});
 
-	public function test_collect_with_array_of_objects_using_closure()
-	{
-		$this->assert_equals(array("0a","1a"),ActiveRecord\collect($this->object_array,function($obj) { return $obj->a; }));
-	}
+			should("work using a closure", function() {
+				assert_equals(array("0a","1a"),AR\collect(transient()->object_array,function($obj) { return $obj->a; }));
+			});
 
-	public function test_collect_with_array_of_objects_using_string()
-	{
-		$this->assert_equals(array("0a","1a"),ActiveRecord\collect($this->object_array,"a"));
-	}
+			should("work using a string", function() {
+				assert_equals(array("0a","1a"),AR\collect(transient()->object_array,"a"));
+			});
+		});
 
-	public function test_collect_with_array_hash_using_closure()
-	{
-		$this->assert_equals(array("0a","1a"),ActiveRecord\collect($this->array_hash,function($item) { return $item["a"]; }));
-	}
+		context("with an array hash", function() {
+			setup(function() {
+				transient()->array_hash = array(
+					array("a" => "0a", "b" => "0b"),
+					array("a" => "1a", "b" => "1b"));
+			});
 
-	public function test_collect_with_array_hash_using_string()
-	{
-		$this->assert_equals(array("0a","1a"),ActiveRecord\collect($this->array_hash,"a"));
-	}
+			should("work using a closure", function() {
+				assert_equals(array("0a","1a"),AR\collect(transient()->array_hash,function($item) { return $item["a"]; }));
+			});
 
-    public function test_array_flatten()
-    {
-		$this->assert_equals(array(), ActiveRecord\array_flatten(array()));
-		$this->assert_equals(array(1), ActiveRecord\array_flatten(array(1)));
-		$this->assert_equals(array(1), ActiveRecord\array_flatten(array(array(1))));
-		$this->assert_equals(array(1, 2), ActiveRecord\array_flatten(array(array(1, 2))));
-		$this->assert_equals(array(1, 2), ActiveRecord\array_flatten(array(array(1), 2)));
-		$this->assert_equals(array(1, 2), ActiveRecord\array_flatten(array(1, array(2))));
-		$this->assert_equals(array(1, 2, 3), ActiveRecord\array_flatten(array(1, array(2), 3)));
-		$this->assert_equals(array(1, 2, 3, 4), ActiveRecord\array_flatten(array(1, array(2, 3), 4)));
-	}
+			should("work using a string", function() {
+				assert_equals(array("0a","1a"),AR\collect(transient()->array_hash,"a"));
+			});
+		});
+	});
 
-	public function test_all()
-	{
-		$this->assert_true(ActiveRecord\all(null,array(null,null)));
-		$this->assert_true(ActiveRecord\all(1,array(1,1)));
-		$this->assert_false(ActiveRecord\all(1,array(1,'1')));
-		$this->assert_false(ActiveRecord\all(null,array('',null)));
-	}
+	should("flatten an array", function() {
+		assert_equals(array(), AR\array_flatten(array()));
+		assert_equals(array(1), AR\array_flatten(array(1)));
+		assert_equals(array(1), AR\array_flatten(array(array(1))));
+		assert_equals(array(1, 2), AR\array_flatten(array(array(1, 2))));
+		assert_equals(array(1, 2), AR\array_flatten(array(array(1), 2)));
+		assert_equals(array(1, 2), AR\array_flatten(array(1, array(2))));
+		assert_equals(array(1, 2, 3), AR\array_flatten(array(1, array(2), 3)));
+		assert_equals(array(1, 2, 3, 4), AR\array_flatten(array(1, array(2, 3), 4)));
+	});
 
-	public function test_classify()
-	{
-		$bad_class_names = array('ubuntu_rox', 'stop_the_Snake_Case', 'CamelCased', 'camelCased');
-		$good_class_names = array('UbuntuRox', 'StopTheSnakeCase', 'CamelCased', 'CamelCased');
+	context("all", function() {
+		should("return true if all elements in an array match", function() {
+			assert_true(AR\all(null,array(null,null)));
+			assert_true(AR\all(1,array(1,1)));
+		});
 
-		$class_names = array();
-		foreach ($bad_class_names as $s)
-			$class_names[] = AR\classify($s);
+		should("return false if all elements do not match", function() {
+			assert_false(AR\all(1,array(1,'1')));
+			assert_false(AR\all(null,array('',null)));
+		});
+	});
 
-		$this->assert_equals($class_names, $good_class_names);
-	}
+	context("classify", function() {
+		should("work", function() {
+			assert_equals("UbuntuRox", AR\classify("ubuntu_rox"));
+			assert_equals("StopTheSnakeCase", AR\classify("stop_the_Snake_Case"));
+			assert_equals("CamelCased", AR\classify("CamelCased"));
+			assert_equals("CamelCased", AR\classify("camelCased"));
+		});
 
-	public function test_classify_singularize()
-	{
-		$bad_class_names = array('events', 'stop_the_Snake_Cases', 'angry_boxes', 'Mad_Sheep_herders', 'happy_People');
-		$good_class_names = array('Event', 'StopTheSnakeCase', 'AngryBox', 'MadSheepHerder', 'HappyPerson');
-
-		$class_names = array();
-		foreach ($bad_class_names as $s)
-			$class_names[] = AR\classify($s, true);
-
-		$this->assert_equals($class_names, $good_class_names);
-	}
-};
+		should("singularize it", function() {
+			assert_equals("Event", AR\classify("events",true));
+			assert_equals("HappyPerson", AR\classify("happy_People",true));
+		});
+	});
+});
 ?>
