b6ee4b88fac59d4874fa577dd7a79d1b4a5bba5c
root <root@localhost.localdomain>
2010-02-08 00:45:40 -0500

    Test to git .gitignore.

--- test/helpers/config.php
+++ test/helpers/config.php
@@ -15,10 +15,11 @@ ActiveRecord\Config::initialize(function($cfg)
 {
 	$cfg->set_model_directory(realpath(dirname(__FILE__) . '/../models'));
 	$cfg->set_connections(array(
-		'mysql'		=> 'mysql://test:test@127.0.0.1/test',
-		'pgsql'		=> 'pgsql://test:test@127.0.0.1/test',
-		'oci'		=> 'oci://test:test@127.0.0.1/xe',
-		'sqlite'	=> 'sqlite://test.db'));
+		'mysql'		=> 'mysql://root:root@127.0.0.1/ar_test',
+//		'pgsql'		=> 'pgsql://test:test@127.0.0.1/test',
+//		'oci'		=> 'oci://test:test@127.0.0.1/xe',
+//		'sqlite'	=> 'sqlite://test.db'));
+);
 	$cfg->set_default_connection('mysql');
 });
 ?>
